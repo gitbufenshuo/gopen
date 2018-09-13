330403bec1f3554acaa5fe34f55faa4aa3061a19
gitbufenshuo <boaitianxia@163.com>
2018-09-13 21:43:10 +0800

    gradually from black to red

--- main.go
+++ main.go
@@ -2,11 +2,13 @@ package main
 
 import (
 	"runtime"
+
+	"github.com/gitbufenshuo/gopen/media/graph"
 )
 
 func init() {
 	runtime.LockOSThread()
 }
 func main() {
-	// graph.Init()
+	graph.Init(800, 600)
 }
--- media/graph/graph.go
+++ media/graph/graph.go
@@ -3,6 +3,8 @@ package graph
 import (
 	"fmt"
 	"log"
+	"math"
+	"time"
 
 	"github.com/go-gl/gl/v4.1-core/gl"
 	"github.com/go-gl/glfw/v3.2/glfw"
@@ -32,5 +34,17 @@ func Init(windowWidth, windowHeight int) {
 
 	version := gl.GoStr(gl.GetString(gl.VERSION))
 	fmt.Println("OpenGL version", version)
-
+	var r float32
+	var frame_number int
+	for !window.ShouldClose() {
+		time.Sleep(time.Millisecond * 30)
+		r = float32(math.Sin(math.Pi*float64((frame_number*2)%1000)/500))/2 + 0.5
+		fmt.Println(r)
+		gl.ClearColor(r, 0, 0, 1)
+		gl.Clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT)
+		// Maintenance
+		window.SwapBuffers()
+		glfw.PollEvents()
+		frame_number++
+	}
 }
