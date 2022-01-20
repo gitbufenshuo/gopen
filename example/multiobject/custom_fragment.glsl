6401958c81cf41c850bfa6d79149c097d5e0db8e
gitbufenshuo <boaitianxia@163.com>
2022-01-20 14:46:56 +0800

    tag

--- example/multiobject/custom_fragment.glsl
+++
@@ -1,9 +0,0 @@
-
-#version 330
-
-out vec4 outputColor;
-uniform float frame;
-
-void main() {
-    outputColor = vec4(frame, 0, 0, 1.0);
-}
--- example/multiobject/custom_vertex.glsl
+++
@@ -1,9 +0,0 @@
-
-#version 330
-
-in vec3 vert;
-uniform float frame;
-
-void main() {
-    gl_Position = vec4(vert.x + frame, vert.y * frame, vert.z, 1.0);
-}
