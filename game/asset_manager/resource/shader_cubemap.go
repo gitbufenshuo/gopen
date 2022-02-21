2071496c34e3ddf1468dc3ec9f277256c81a7aad
gitbufenshuo <boaitianxia@163.com>
2022-02-21 17:25:08 +0800

    some png

--- game/asset_manager/resource/shader_cubemap.go
+++ game/asset_manager/resource/shader_cubemap.go
@@ -11,23 +11,23 @@ var ShaderCubeMapText ShaderText = ShaderText{
 	layout (location = 0) in vec3 vert;
 	uniform mat4 rotation;
 	
-	out vec3 fragVertNormal;
+	out vec3 textureDir;
 	
 	void main() {
 		vec4 wNormal = rotation * vec4(vert, 1);
-		fragVertNormal = vert;
+		textureDir = vert;
 		gl_Position = wNormal.xyww;
 	}`,
 	Fragment: `#version 330
 
 	uniform samplerCube tex;
 	
-	in vec3 fragVertNormal;
+	in vec3 textureDir;
 	
 	out vec4 outputColor;
 	
 	void main() {
 		// outputColor = vec4(1,0,0,1);
-		outputColor = texture(tex, fragVertNormal);
+		outputColor = texture(tex, textureDir);
 	}`,
 }
